6206df0bbd70b49ce719dcc63f791493d98f4596
cindyeme <emereninicynthia@gmail.com>
2022-11-04 10:44:50 +0100

    added autoHide scrollbar on sidenav

--- pages/index.jsx
+++ pages/index.jsx
@@ -25,7 +25,7 @@ export default function Home() {
           <SideNav sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
           {/* Content area */}
-          <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden bg-black-100">
+          <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden bg-black-100 border-l">
             <main>
               <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
                 {/* Welcome banner */}
